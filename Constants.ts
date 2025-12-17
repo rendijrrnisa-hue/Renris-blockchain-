cb878116547da6dd1a2de26c46fb18beb785155c
rendijrrnisa-hue <rendijrrnisa@gmail.com>
2025-12-18 03:53:18 +0700

    Add constants for token details and rewards

---
+++ Constants.ts
@@ -0,0 +1,12 @@
+export const TOKEN_DETAILS = {
+  name: "Ren",
+  symbol: "REN",
+  totalSupply: 200000000,
+  marketAllocation: 100000000,
+  miningAllocation: 100000000,
+  developer: "Dwi Frendi Priyanto",
+  blockchain: "Renris",
+};
+export const REWARD_PER_24H = 1;
+export const DEV_TAX_PERCENT = 20;
+export const REWARD_PER_SECOND = REWARD_PER_24H / 86400;
